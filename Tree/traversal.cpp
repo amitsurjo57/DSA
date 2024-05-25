08b8604991ae9d6e39667ac942087f9e6007a5fa
Amit Banik Surjo <155183598+amitsurjo57@users.noreply.github.com>
2024-05-25 19:23:59 +0600

    DSA

---
+++ Tree/traversal.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void preorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void postorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+/*
+
+          1
+        /   \
+       2     3
+      / \   / \
+     4   5 6   7
+
+*/
+
+int main()
+{
+    Node *root = new Node(1);
+
+    root->left = new Node(2);
+    root->right = new Node(3);
+
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    cout << "Pre Order: ";
+    preorder(root);
+    cout << endl;
+
+    cout << "In Order: ";
+    inorder(root);
+    cout << endl;
+
+    cout << "Post Order: ";
+    postorder(root);
+    cout << endl;
+}
