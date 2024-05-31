a1b48b3192224c3a3246f397bf7029fe51340d63
Amit Banik Surjo <155183598+amitsurjo57@users.noreply.github.com>
2024-05-31 11:53:25 +0600

    DSA

---
+++ BinarySearch.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int l, int key)
+{
+    int si = 0;
+    int ei = l;
+    while (si <= ei)
+    {
+        int mid = (si + ei) / 2;
+
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        else if (arr[mid] > key)
+        {
+            ei = mid - 1;
+        }
+        else
+        {
+            si = mid + 1;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+    int key;
+    cout << "Enter the key: ";
+    cin >> key;
+
+    cout << "Position: " << binarySearch(arr, 9, key);
+}
